dc3636711138d9ef8ee636e1c635cf797d6f1f8f
waseem4100 <waseemahmed4100@gmail.com>
2024-07-02 12:16:21 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([{
+    type: "number",
+    name : "Firstnumber",
+    message : "Enter your first number",
+    },
+    {type: "number" ,
+    name : "Secondnumber",
+    message : "Enter your second number",
+    },
+    {
+        name : "Operator",
+        type: "list",
+        message: "Choose an operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+        
+        
+        
+    }
+    
+])
+
+// conditional operators
+
+ if (answer.Operator === "Addition") {
+    console.log(answer.Firstnumber + answer.Secondnumber)
+}
+else if(answer.Operator === "Substraction") {
+    console.log(answer.Firstnumber - answer.Secondnumber)
+}
+
+else if (answer.Operator === "Multiplication") {
+    console.log(answer.Firstnumber * answer.Secondnumber)
+}
+else if (answer.Operator === "Division") {
+    console.log(answer.Firstnumber / answer.Secondnumber)
+}
+else (
+    console.log("please select a valid operator")
+)
+
+
